c677eb9ffe44ffff9818e9006f6d2ed716365bdb
Victor Ribeiro <victor.ribeiro97@gmail.com>
2024-04-24 18:57:46 -0300

    aula 2

---
+++ OperadoresMatematicos.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication3;
+import java.lang.Math;
+/**
+ *
+ * @author vic_r
+ */
+ public class OperadoresMatematicos {
+   
+    public static void main(String[] args) {
+        int copo = 1;
+    int celular = 2;
+        System.out.println(copo*celular);
+    }
+ public static void teste (String[] args){
+  //System.out.println(copo*celular);
+  /*
+  *
+  */
+ }
+}
